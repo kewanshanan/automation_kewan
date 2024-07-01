e8b9b82801310583054fbdbb1b9c36b6eaa685cf
Kewan Shanan <kewan101@gmail.com>
2024-07-01 18:14:41 +0300

    task2

--- intro_to_selenium/task2.py
+++ intro_to_selenium/task2.py
@@ -1,18 +1,21 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-
-#infra
+#infra - העמדה
 driver = webdriver.Chrome()
 
-#infra       #logic
-driver.get("https://www.youtube.com//")
+#infra        #logic
+driver.get('https://www.google.com')
 
 #logic
 search_input = driver.find_element(By.XPATH, '//input[@id="Search"]')
-search_input.send_keys("barcelona")
+search_input.send_keys('Python programming')
 search_input.send_keys(Keys.RETURN)
+time.sleep(2)
+print(driver.title)
 
 
 #infra
