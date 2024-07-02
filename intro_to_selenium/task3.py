470ba3dab76dfa53e77174eb0bff243704400b48
Kewan Shanan <kewan101@gmail.com>
2024-07-02 13:29:11 +0300

    update

--- intro_to_selenium/task3.py
+++ intro_to_selenium/task3.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-#first
+#first-test
 driver = webdriver.Chrome()
 
 driver.get('https://ultimateqa.com/simple-html-elements-for-automation/')
@@ -13,7 +13,7 @@ print(Simple_Controls.text)
 
 driver.quit()
 
-#second
+#second-test
 driver = webdriver.Chrome()
 driver.get('https://ultimateqa.com/simple-html-elements-for-automation/')
 Simple_Controls = driver.find_element(By.XPATH, '//*[@id="et_pb_contact_name_0"]')
@@ -26,5 +26,18 @@ Simple_Controls.send_keys('kewan101@gmail.com')
 Simple_Controls.send_keys(Keys.RETURN)
 Simple_Controls = driver.find_element(By.XPATH, '//*[@id="et_pb_contact_form_0"]/div[2]/form/div/button')
 Simple_Controls.click()
+
+driver.quit()
+
+
+#third-test
+driver = webdriver.Chrome()
+driver.get('https://ultimateqa.com/complicated-page')
+buttons_count = len(driver.find_elements(By.XPATH, "//a[contains(text(), 'Button')]"))
+print(f"there is {buttons_count} in this page")
+if buttons_count == 12:
+    print("test pass")
+else:
+    print("test fail")
 
 driver.quit()
