1fb483e85e207c7a41268bbc53b612b7eabb5729
Kewan Shanan <kewan101@gmail.com>
2024-07-02 11:05:43 +0300

    update

--- intro_to_selenium/task3.py
+++ intro_to_selenium/task3.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+#first
 driver = webdriver.Chrome()
 
 driver.get('https://ultimateqa.com/simple-html-elements-for-automation/')
@@ -12,4 +13,18 @@ print(Simple_Controls.text)
 
 driver.quit()
 
+#second
+driver = webdriver.Chrome()
+driver.get('https://ultimateqa.com/simple-html-elements-for-automation/')
+Simple_Controls = driver.find_element(By.XPATH, '//*[@id="et_pb_contact_name_0"]')
+Simple_Controls.click()
+Simple_Controls.send_keys('kewan')
+Simple_Controls.send_keys(Keys.RETURN)
+Simple_Controls = driver.find_element(By.XPATH, '//*[@id="et_pb_contact_email_0"]')
+Simple_Controls.click()
+Simple_Controls.send_keys('kewan101@gmail.com')
+Simple_Controls.send_keys(Keys.RETURN)
+Simple_Controls = driver.find_element(By.XPATH, '//*[@id="et_pb_contact_form_0"]/div[2]/form/div/button')
+Simple_Controls.click()
 
+driver.quit()
